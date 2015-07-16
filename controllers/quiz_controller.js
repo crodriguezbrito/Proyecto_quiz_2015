66fb364b11fb2381ab12abf16986cac8f95a607f
carlos <crodriguezbrito@gmail.com>
2015-07-16 13:16:06 +0100

    Quiz 7B sequelize.js y Sqlite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,20 @@
+var models = require('../models/models.js');
+
+//Cambiamos then por success //version de sequelize mas actualizada
 // Get /quizes/question
 exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().then(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta})
+	})
 };
 
 //Get /quizes/answer
 exports.answer = function(req, res){
-	if(req.query.respuesta === 'Roma'){
-		res.render('quizes/answer' , {respuesta: 'Correcto'});
-	}else{
-		res.render('quizes/answer' , {respuesta: 'Incorrecta'});
-	}
+	models.Quiz.findAll().then(function(quiz) {
+		if(req.query.respuesta === quiz[0].respuesta){
+			res.render('quizes/answer' , {respuesta: 'Correcto'});
+		}else{
+			res.render('quizes/answer' , {respuesta: 'Incorrecta'});
+		}
+	})
 }; 
