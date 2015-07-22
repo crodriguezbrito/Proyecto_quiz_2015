218706bc5b446f67728896349f54fb1a34395010
carlos <crodriguezbrito@gmail.com>
2015-07-22 10:59:57 +0100

    quiz 10 Autoload

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz' });
 });
 
+//Autoload de comandos con quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
+
+//Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
