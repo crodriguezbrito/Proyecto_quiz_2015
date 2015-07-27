9113ee167eb4ae9397602edcdb638ecafcd69fa8
carlos <crodriguezbrito@gmail.com>
2015-07-27 13:21:04 +0100

    quiz13 Editar preguntas

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials'); //instalamos middleware en app.js ***
+var methodOverride = require('method-override')
 var routes = require('./routes/index');
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
