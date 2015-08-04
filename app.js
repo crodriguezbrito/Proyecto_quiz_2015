ec27b8ed227af856e2140e8dfd1ab3996c80634d
carlos <crodriguezbrito@gmail.com>
2015-08-04 19:21:03 +0100

    Quiz 16 Autentificacion y sesion

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials'); //instalamos middleware en app.js ***
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+var session = require('express-session');
+
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -23,9 +26,24 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Middleware Helpers dinamicos:
+app.use(function(req, res, next) {
+    //guardar en path en session.redir para despues de login
+    if (!req.path.match(/\/login|\/logout/)) {
+        req.session.redir = req.path;
+    }
+
+    //hacer visible req.session en las vistas
+    res.locals.session = req.session;
+    next();
+});
+
+
 
 app.use('/', routes);
 
